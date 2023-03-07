eece1305e58af21f362f7661b148786c2f8f5242
victordiasdff <victordiasdff@gmail.com>
2023-03-07 17:00:32 -0300

    Usando Dto para retornar usuarios

---
+++ src/main/java/com/victordiasdff/workshopmongo/dto/UserDTO.java
@@ -0,0 +1,50 @@
+package com.victordiasdff.workshopmongo.dto;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+
+import com.victordiasdff.workshopmongo.domain.User;
+
+public class UserDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	private String id;
+	private String name;
+	private String email;
+	
+	public UserDTO() {
+	}
+	
+	public UserDTO(User obj) {
+		id = obj.getId();
+		name = obj.getName();
+		email = obj.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
